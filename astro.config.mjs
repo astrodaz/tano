0253c2746f984369b5581f5eb2b4f2003c6cde29
Daz <mail@darrenjehan.me.uk>
2026-03-15 15:07:22 +0000

    Remove Vercel adapter, configure for static output

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: vercel(),
   integrations: [tailwind()],
   server: {
     port: 4321,
