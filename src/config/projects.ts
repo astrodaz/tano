6cd8b0e64929898405007b87a58434798c4e3fa4
Daz <mail@darrenjehan.me.uk>
2026-04-03 17:42:56 +0100

    Restricted gallery to only show current section images in Projects.

--- src/config/projects.ts
+++ src/config/projects.ts
@@ -18,17 +18,68 @@ export const projectsContent = {
           href: "#",
         },
         {
-          title: "M31 - Andromeda Galaxy",
-          description:
-            "Mosaic project imaging the Andromeda Galaxy.",
-          image: "/assets/images/projects/061022_m31.jpg",
+          title: "",
+          description: "",
+          image: "/assets/images/projects/0904_bubble_rework.jpg",
           href: "#",
         },
         {
-          title: "M33 - Triangulum Galaxy",
-          description:
-            "The Triangulum Galaxy captured in LRGB.",
-          image: "/assets/images/projects/M33LRGB.jpg",
+          title: "",
+          description: "",
+          image: "/assets/images/projects/Caldwell_C19.jpg",
+          href: "#",
+        },
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/gamma_cass.jpg",
+          href: "#",
+        },
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/Ha_v1.png",
+          href: "#",
+        },
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/Horsehead_Col.jpg",
+          href: "#",
+        },
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/Merope_LRGB_3.jpg",
+          href: "#",
+        },
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/NGC2239_pi1.jpg",
+          href: "#",
+        },
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/NGC7380.png",
+          href: "#",
+        },
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/NGC_7635_Colour.jpg",
+          href: "#",
+        },
+      ]
+    },
+    {
+      header: "Solar System",
+      items: [
+        {
+          title: "Moon - March 2017",
+          description: "Moon from early March, taken in 2017",
+          image: "/assets/images/projects/0313_Moon.png",
           href: "#",
         },
         {
@@ -58,12 +109,6 @@ export const projectsContent = {
         {
           title: "",
           description: "",
-          image: "/assets/images/projects/080929_m57.jpg",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
           image: "/assets/images/projects/081016_Moon.jpg",
           href: "#",
         },
@@ -76,7 +121,7 @@ export const projectsContent = {
         {
           title: "",
           description: "",
-          image: "/assets/images/projects/0904_bubble_rework.jpg",
+          image: "/assets/images/projects/holmes_071207.jpg",
           href: "#",
         },
         {
@@ -91,10 +136,69 @@ export const projectsContent = {
           image: "/assets/images/projects/090625_crescent2.jpg",
           href: "#",
         },
+      ]
+    },
+    {
+      header: "Galaxies",
+      items: [
         {
           title: "",
           description: "",
-          image: "/assets/images/projects/Caldwell_C19.jpg",
+          image: "/assets/images/projects/NGC_891_LRGB.jpg",
+          href: "#",
+        },
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/M51_LRGB_clone.png",
+          href: "#",
+        },
+        {
+          title: "M33 - Triangulum Galaxy",
+          description:
+            "The Triangulum Galaxy captured in LRGB.",
+          image: "/assets/images/projects/M33LRGB.jpg",
+          href: "#",
+        },
+        {
+          title: "M31 - Andromeda Galaxy",
+          description:
+            "Mosaic project imaging the Andromeda Galaxy.",
+          image: "/assets/images/projects/061022_m31.jpg",
+          href: "#",
+        },
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/NGC7331.jpg",
+          href: "#",
+        },
+      ]
+    }, 
+    {
+      header: "Planetary Nebula",
+      items: [
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/080929_m57.jpg",
+          href: "#",
+        },
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/m57_kelling_proc_2.jpg",
+          href: "#",
+        },
+      ]
+    },  
+    {
+      header: "Star Clusters",
+      items: [
+        {
+          title: "",
+          description: "",
+          image: "/assets/images/projects/m3_final.jpg",
           href: "#",
         },
         {
@@ -106,100 +210,11 @@ export const projectsContent = {
         {
           title: "",
           description: "",
-          image: "/assets/images/projects/gamma_cass.jpg",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/Ha_v1.png",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/holmes_071207.jpg",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/Horsehead_Col.jpg",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
           image: "/assets/images/projects/m38.jpg",
           href: "#",
         },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/m3_final.jpg",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/M51_LRGB_clone.png",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/m57_kelling_proc_2.jpg",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/Merope_LRGB_3.jpg",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/NGC2239_pi1.jpg",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/NGC7331.jpg",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/NGC7380.png",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/NGC_7635_Colour.jpg",
-          href: "#",
-        },
-        {
-          title: "",
-          description: "",
-          image: "/assets/images/projects/NGC_891_LRGB.jpg",
-          href: "#",
-        },
       ]
-    },
-    {
-      header: "Solar System",
-      items: [
-        {
-          title: "Moon - March 2017",
-          description: "Moon from early March, taken in 2017",
-          image: "/assets/images/projects/0313_Moon.png",
-          href: "#",
-        },
-      ]
-    }
+    },     
   ]
 };
 
